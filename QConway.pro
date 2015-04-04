f9c8256a4556532f991e792e1b611b4c45078ea1
Dmitriy Belkin <demondima123456@gmail.com>
2015-04-04 15:01:32 +0300

    Initial commit

---
+++ QConway.pro
@@ -0,0 +1,8 @@
+QT += core gui widgets
+
+TEMPLATE = app
+TARGET = QConway
+
+CONFIG += -std=c++1y
+
+SOURCES += main.cpp
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <QApplication>
+
+#include "iostream"
+
+int main(int argc, char const *argv[])
+{
+	std::cout << "hello" << std::endl;
+	return 1;
+}
