52aa2ecf7dae97837caedf3d0060eb98c440841f
Dmitriy Belkin <demondima123456@gmail.com>
2015-04-11 15:40:39 +0300

    [main.cpp] Added simple test

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 #include <QApplication>
 #include <QDebug>
 
+#include "iostream"
+
 #include "config.h"
+#include "life.h"
+
+using namespace std;
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -11,7 +16,29 @@ int main(int argc, char *argv[]) {
     tmp->test = "new foo";
     Config::setConfig(tmp);
 
-    qDebug() << Config::getConfig()->test;
+    qDebug() << Config::getConfig()->test << endl;
+
+    Life life(10, 10);
+    life.setCell(0, 0, true);
+    life.setCell(0, 1, true);
+    life.setCell(0, 2, true);
+    life.setCell(2, 1, true);
+    life.setCell(1, 2, true);
+
+    int N = 20;
+    while (N-->0) {
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < 10; ++j) {
+                cout << (life.getCell(i, j) ? '0' : '.');
+            }
+            cout << endl;
+        }
+
+        cout << endl << endl;
+
+        life.step();
+    }
+
 
     return app.exec();
 }
